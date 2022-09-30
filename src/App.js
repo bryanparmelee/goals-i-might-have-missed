447215917f13259072c4e593ea1589f3f1ee345d
bryanparmelee <bryan.parmelee@gmail.com>
2022-09-29 21:48:35 -0400

    Add API call, intial sort of leagues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+
+import { initialLeagueRegex } from "./utils/leagueRegex";
+
 const App = () => { 
+  const [matches, setMatches] = useState([]);
+
+  const token = process.env.REACT_APP_SCOREBAT_TOKEN;
+  const api = `https://www.scorebat.com/video-api/v3/feed/?token=${token}`;
+
+    fetch(api)
+      .then(res => res.json())
+      .then(data => {
+        const sortedLeagues = data.response.filter((val) => val.competition.match(initialLeagueRegex));
+        console.log(sortedLeagues);
+      })
+      .catch(error => console.log(error))
+ 
+
+
+
   return (
     <div>
       Hello world
---
+++ src/utils/leagueRegex.js
@@ -0,0 +1,2 @@
+export const initialLeagueRegex = /SPAIN:\sLa\sLiga\b|GERMANY:\sBundesliga|FRANCE:\sLigue\s1|ITALY:\sSerie\sA|ENGLAND:\sPremier\sLeague\b(?!\d)|^CHAMPIONS\sLEAGUE|EUROPA\sLEAGUE|ENGLAND:\sFA\sCup|WORLD/;
+
